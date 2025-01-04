f848bf04e31599d44f49549f0bfa6544afa75117
Aman Gupta <coderamang02@gmail.com>
2025-01-05 00:22:14 +0530

    changes in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import {App} from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
 
-<BrowserRouter>
+<HashRouter>
   <Toaster/>
   <App />
-  </BrowserRouter>
+  </HashRouter>
 
 )
